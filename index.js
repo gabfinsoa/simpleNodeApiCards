eecf304d4478d67699e74c12a63e1f295ff5e76e
Gabriel Soares <gssoares@arezzo.com.br>
2019-12-06 23:29:00 -0300

    Add para o browser não bloquear a requisicao: res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE'); res.header('Access-Control-Allow-Headers', 'Content-Type');

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ server.use(express.json());
 //Middleware
 server.use((req,res,next) => {
     res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
 
