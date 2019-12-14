e1f131be41deb6674f67416b1d442cdb40a4b6b5
Gabriel Soares <gssoares@arezzo.com.br>
2019-12-14 17:23:09 -0300

    Ajustada API

--- database.js
+++ database.js
@@ -1,9 +1,10 @@
 
 //add conexão com o banco de dados
-//importar a classe Sequelize
+//importar a classe Sequelize (obs.: ao importar uma classe, a mesma deve ser em maiusculo)
 const Sequelize = require('sequelize');
 
-//rotas, puxar as configurações/variaveis criadas no dotenv/heroku
+//rotas, puxar as configurações/variaveis criadas na api e também no heroku
+//usado para pegar as variaveis do arquivo de configuracao (.env)
 require('dotenv').config();
 
 //pegar URL no Heroku
@@ -11,11 +12,13 @@ require('dotenv').config();
 //Criar a conexão com o banco de dados
 //                                     URL no env
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    //especificar o tipo de banco a ser conectado
     dialect: 'postgres',
     dialectOptions: {
+    //especificar que é uma conexao segura 
     ssl: true
     }
-    });
+});
 
-
+//exportar a classe
 module.exports = sequelize;
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-
+//importando a biblioteca do express
 const express = require('express');
 
-//importar o banco de dados
+//arquivo para importar/configurar o banco de dados
+//importando a classe database.js
 const database = require('./database');
 
 // const dotenv = require('dotenv');
@@ -12,15 +13,15 @@ const server = express();
 server.use(express.json());
 
 //Middleware
-server.use((req,res,next) => {
-    res.header('Access-Control-Allow-Origin','*');
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     //console.log(process.env.TESTE);
     next();
 });
 
-//Middleware
+//Middleware para verificar se o card existe
 function checkCard(req,res,next){
     const {id} = req.params;
     // Encontrando um CARD no array
@@ -34,18 +35,24 @@ function checkCard(req,res,next){
     next();
 }
 
-//Criar middleware para ver qual é o ultimo ID
-function lastId(req,res,next){
-database.query(`SELECT MAX(id) FROM cards`, {type: database.QueryTypes.SELECT})
-    .then(results => {
-        console.log("results");
+//Middleware para ver qual é o ultimo ID
+async function getNextId(req, res, next){
+    //Query para pegar o maior ID
+    await database.query(`SELECT MAX(id) FROM cards`, { type: database.QueryTypes.SELECT })
+    .then(id => {
+        console.log(results);
+        
+        //Atribuir o resultado da query MAX para o NextID
+        nextId = id[0].max;
         
     })
+    // nextId++;
+    //Ir para a próxima execucao/requisicao (funcao/classe)
     next();
 }
 
 //criar 2 variaveis por não haver banco de dados
-let nextId = 1;
+let nextId = null;
 let cards = [] //array de cards (objetos)
 
 //ROUTES
@@ -54,12 +61,13 @@ server.get('/',(req,res) => {
     return res.json({result: "API-SCRAPBOOK"});
 });
 
-server.get("/cards", lastId, (req,res) => {
-    //conectar a banco e fazer o select. E qual o tipo de resposta que quero ao executar o comando (select)
-    database.query(`SELECT * FROM cards`, {type: database.QueryTypes.SELECT})
-    .then(results => {
-        console.log("results");
-        cards = results;
+server.get("/cards",  async (req,res) => {
+    //conectar ao banco e fazer o select. E qual o tipo de resposta que quero ao executar o comando (select)
+    //utilizando a funcao QUERY do database: Select + Tipod e Query na resposta
+   await database.query(`SELECT * FROM cards`, {type: database.QueryTypes.SELECT})
+    .then(resultCards => {
+        // console.log(results);
+        cards = resultCards;
         console.log(cards);
     })
 
@@ -67,7 +75,9 @@ server.get("/cards", lastId, (req,res) => {
 });
 
 //req pega as coisas que o cliente envia
-server.post("/cards", (req, res) => {
+server.post("/cards", getNextId, (req, res) => {
+    //Incrementar o ID
+    nextId++;
     // A requisicao pega no <body> os campos TITLE e CONTENT
     const {title, content} = req.body;
     
@@ -78,11 +88,20 @@ server.post("/cards", (req, res) => {
         content
     };
 
+    //Realizando INSERT no banco
+    database.query(`INSERT INTO cards ("id", "title", "content") VALUES (${nextId},'${title}','${content}')`,
+        { type: database.QueryTypes.INSERT }
+    )
+    .then(resultInsert => {
+        console.log(resultInsert);        
+        
+    });
+
     // Adicionar cada card no array de CARDS
     cards.push(card);
 
-    // Incrementar o ID
-    nextId++;
+    // // Incrementar o ID
+    // nextId++;
 
     return res.json(card);
 });
@@ -95,6 +114,8 @@ server.put("/cards/:id", checkCard, (req, res) => {
     //Pegar do BODY as variaveis TITLE e CONTENT para editar
     const {title, content} = req.body;
 
+    let updated = false;
+
     // Encontrando um CARD no array
     const card = cards.find(procuraCard => procuraCard.id == id);
 
@@ -103,12 +124,25 @@ server.put("/cards/:id", checkCard, (req, res) => {
     if(title){
         //Caso o valor exista, add o novo titulo (via postman)
         card.title = title;
+        updated = true;
     }
     //Verificar se contem valor no CONTENT
     if(content){
         //Caso o valor exista, add o novo conteudo (via postman)
         card.content = content;
+        updated = true;
     }
+
+    //Se algo foi atualizado...
+    if(updated) {
+        //Faz update no banco
+        database.query(`UPDATE cards SET content = '${scrap.content}', title = '${scrap.title}' WHERE  id = ${id}`,
+            { type: database.QueryTypes.UPDATE}
+        )
+        .then(update => {
+            console.log(update);
+        });
+    };
 
     return res.json(card);
 });
@@ -120,7 +154,12 @@ server.delete("/cards/:id", checkCard, (req, res) => {
 
     const cardIndex = cards.findIndex(card => card.id == id);
 
-    cards.splice(cardIndex,1);
+    database.query(`DELETE FROM cards WHERE id = ${id};`, { type: database.QueryTypes.DELETE})
+    .then(del => {
+        console.log(del);
+    });
+
+    cards.splice(cardIndex, 1);
 
     return res.json(cards);
 });
